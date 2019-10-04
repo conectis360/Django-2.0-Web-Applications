ff82b18dc0f476bb91203d609262c20a6537ab1a
conectis360 <conectis360@gmail.com>
2019-10-04 11:57:18 -0300

    Creating some new models for the relationship between movies and persons

--- core/models.py
+++ core/models.py
@@ -1,5 +1,39 @@
 from django.db import models
 
+class PersonManager(models.Manager):
+    def all_with_prefetch_movies(self):
+        qs = self.get_queryset()
+        return qs.prefech_related(
+            'directed',
+            'writing_credits',
+            'role_set_movie'
+        )
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=140)
+    last_name = models.CharField(max_length=140)
+    born = models.DateField()
+    died = models.DateField(null=True, blank=True)
+    
+    objects = PersonManager()
+    
+    class Meta:
+        ordering = ('last_name', 'first_name')
+        
+    def __str__(self):
+        if self.died:
+            return '{}, {}({} - {})'.format(
+                self.last_name,
+                self.first_name,
+                self.born,
+                self.died
+            )
+        return'{}, {} ({})'.format (
+            self.last_name,
+            self.first_name,
+            self.born
+        )
+
 class Movie(models.Model):
     NOT_RATED = 0
     RATED_G = 1
@@ -31,3 +65,41 @@ class Movie(models.Model):
     def __str__(self):
         return '{} ({})'.format(
             self.title, self.year)
+        
+    director = models.ForeignKey(
+        to='Person',
+        on_delete=models.SET_NULL,
+        related_name='directed',
+        null=True,
+        blank=True
+    )
+    
+    writers = models.ManyToManyField(
+        to='Person',
+        related_name='writing_credits',
+        blank=True
+    )
+    
+    actors = models.ManyToManyField(
+        to='Person',
+        through='Role',
+        related_name='acting_credits',
+        blank=True
+    )
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+    person = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=140)
+    
+    def __str__(self):
+        return "{} {} {}".format(self.movie_id, self.person_id, self.name)
+    
+    class Meta:
+        unique_together = (
+            'movie',
+            'person',
+            'name'
+        )
+        
+        
